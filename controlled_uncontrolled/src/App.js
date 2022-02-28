433f5785ece2cc4baef6f67213a090acc4b45192
LeoKnox <LeoKnox@users.noreply.github.com>
2022-02-28 11:05:45 -0800

    Add files via upload

--- controlled_uncontrolled/src/App.js
+++ controlled_uncontrolled/src/App.js
@@ -1,9 +1,23 @@
+import { useState } from 'react';
 import { UncontrolledForm } from './UncontrolledForm';
 import { ControlledForm } from  './ControlledForm';
+import { ControlledModal } from './ControlledModal';
 
 function App() {
+  const [shouldShowModal, setShouldShowModal] = useState(false);
+
 	return (
-    <ControlledForm />
+    <>
+      <ControlledModal
+        shouldShow={shouldShowModal}
+        onRequestClose={() => setShouldShowModal(false)}
+      >
+        <h1>Whee!</h1>
+      </ControlledModal>
+      <button onClick={() => setShouldShowModal(!shouldShowModal)}>
+        {shouldShowModal ? 'Hide Modal' : 'Show Modal'}
+      </button>
+    </>
 	);
 }
 
