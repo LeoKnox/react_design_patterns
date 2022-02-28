593c7340ba7c9a949bea004024322f766ab18018
LeoKnox <LeoKnox@users.noreply.github.com>
2022-02-28 12:17:02 -0800

    Add files via upload

--- controlled_uncontrolled/src/App.js
+++ controlled_uncontrolled/src/App.js
@@ -4,25 +4,28 @@ import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
 const StepOne = ({ goToNext }) =>(
   <>
     <h1>Step 1</h1>
-    <button onClick={goToNext}>Next</button>
+    <button onClick={() => goToNext({ name: "joe jinx"})}>Next</button>
   </>
 ) 
 const StepTwo = ({ goToNext }) =>(
   <>
     <h1>Step 2</h1>
-    <button onClick={goToNext}>Next</button>
+    <button onClick={() => goToNext({ age: 88 })}>Next</button>
   </>
 ) 
 const StepThree = ({ goToNext }) =>(
   <>
     <h1>Step 3</h1>
-    <button onClick={goToNext}>Next</button>
+    <button onClick={() => goToNext({ hairColor: "indigo"})}>Next</button>
   </>
 ) 
 
 function App() {
 	return (
-    <UncontrolledOnboardingFlow>
+    <UncontrolledOnboardingFlow onFinish={data => {
+        console.log(data);
+        alert('steps complete');
+      }}>
       <StepOne />
       <StepTwo />
       <StepThree />
--- controlled_uncontrolled/src/UncontrolledOnboardingFlow.js
+++ controlled_uncontrolled/src/UncontrolledOnboardingFlow.js
@@ -4,8 +4,23 @@ export const UncontrolledOnboardingFlow = ({ children, onFinish }) => {
     const [onBaordingData, setOnboardingData] = useState({});
     const [currentIndex, setCurrentIndex] = useState(0);
 
-    const goToNext = () => {
-        setCurrentIndex(currentIndex + 1);
+    const goToNext = stepData => {
+        const nextIndex = currentIndex + 1;
+
+        const updatedData = {
+            ...onBaordingData,
+            ...stepData,
+        };
+
+        console.log(updatedData);
+
+        if (nextIndex < children.length) {
+            setCurrentIndex(nextIndex);
+        } else {
+            onFinish(updatedData);
+        }
+
+        setOnboardingData(updatedData);
     }
 
     const currentChild = React.Children.toArray(children)[currentIndex];
