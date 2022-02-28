b7e8a012760fec4543275e7e190f22d932acd0db
LeoKnox <LeoKnox@users.noreply.github.com>
2022-02-28 11:50:08 -0800

    Add files via upload

--- controlled_uncontrolled/src/App.js
+++ controlled_uncontrolled/src/App.js
@@ -1,23 +1,32 @@
 import { useState } from 'react';
-import { UncontrolledForm } from './UncontrolledForm';
-import { ControlledForm } from  './ControlledForm';
-import { ControlledModal } from './ControlledModal';
+import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
+
+const StepOne = ({ goToNext }) =>(
+  <>
+    <h1>Step 1</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+) 
+const StepTwo = ({ goToNext }) =>(
+  <>
+    <h1>Step 2</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+) 
+const StepThree = ({ goToNext }) =>(
+  <>
+    <h1>Step 3</h1>
+    <button onClick={goToNext}>Next</button>
+  </>
+) 
 
 function App() {
-  const [shouldShowModal, setShouldShowModal] = useState(false);
-
 	return (
-    <>
-      <ControlledModal
-        shouldShow={shouldShowModal}
-        onRequestClose={() => setShouldShowModal(false)}
-      >
-        <h1>Whee!</h1>
-      </ControlledModal>
-      <button onClick={() => setShouldShowModal(!shouldShowModal)}>
-        {shouldShowModal ? 'Hide Modal' : 'Show Modal'}
-      </button>
-    </>
+    <UncontrolledOnboardingFlow>
+      <StepOne />
+      <StepTwo />
+      <StepThree />
+    </UncontrolledOnboardingFlow>
 	);
 }
 
