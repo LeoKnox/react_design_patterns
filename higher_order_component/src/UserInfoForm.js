602be143d8b1d39e56531c566b490e05b453f9dd
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-01 12:23:22 -0800

    Add files via upload

---
+++ higher_order_component/src/UserInfoForm.js
@@ -0,0 +1,18 @@
+import { withEditableUser } from "./withEditableUser";
+
+export const UserInfoForm = withEditableUser(({ user, onChangeUser, onSaveUser, onResetUser}) => {
+    const { name, age, hairColor } = user || {};
+
+    return user ? (
+        <>
+            <p>Name:</p>
+            <input value={name} onChange={e => onChangeUser({ name: e.target.value })} />
+            <p>Age:</p>
+            <input type = "number" value={age} onChange={e => onChangeUser({ age: Number(e.target.value) })} />
+            <p>HairColor:</p>
+            <input value={hairColor} onChange={e => onChangeUser({ hairColor: e.target.value })} />
+            <button onClick={onResetUser}>Reset</button>
+            <button onClick={onSaveUser}>Save</button>
+        </>
+    ) : <p>Loading</p>;
+}, '123');
