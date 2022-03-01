9c996cec2503886c8de542a02e2c69f2369902fe
LeoKnox <LeoKnox@users.noreply.github.com>
2022-03-01 10:47:37 -0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,44 +1,12 @@
-import { useState } from 'react';
-import { UncontrolledOnboardingFlow } from './UncontrolledOnboardingFlow';
-import { ControlledOnboardingFlow } from './ControlledOnboardingFlow';
+import { printProps } from './printProps';
+import { UserInfo } from './UserInfo';
+import { withUser } from './withUser';
 
-const StepOne = ({ goToNext }) =>(
-  <>
-    <h1>Step 1</h1>
-    <button onClick={() => goToNext({ name: "joe jinx"})}>Next</button>
-  </>
-) 
-const StepTwo = ({ goToNext }) =>(
-  <>
-    <h1>Step 2</h1>
-    <button onClick={() => goToNext({ age: 88 })}>Next</button>
-  </>
-) 
-const StepThree = ({ goToNext }) =>(
-  <>
-    <h1>Step 3</h1>
-    <button onClick={() => goToNext({ hairColor: "indigo"})}>Next</button>
-  </>
-) 
+const UserInfoWithLoader = withUser(UserInfo, '234');
 
 function App() {
-  const [onboardingData, setOnboardingData] = useState({});
-  const [currentIndex, setCurrentIndex] = useState(0);
-
-  const onNext = stepData => {
-    setOnboardingData({ ...onboardingData, ...stepData })
-    setCurrentIndex(currentIndex + 1);
-  }
-
 	return (
-    <ControlledOnboardingFlow 
-      currentIndex ={currentIndex}
-      onNext={onNext}
-    >
-      <StepOne />
-      <StepTwo />
-      <StepThree />
-    </ControlledOnboardingFlow>
+    <UserInfoWithLoader />
 	);
 }
 
